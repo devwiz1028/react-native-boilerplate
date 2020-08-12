97b8acbf784af5f891ec1a8ce082ad644d9f0756
devwiz1028 <devwiz.sh@gmail.com>
2020-08-12 02:13:13 -0700

    add reactnavigation stack navigator

--- src/App.js
+++ src/App.js
@@ -6,24 +6,36 @@
  * @flow strict-local
  */
 
+import 'react-native-gesture-handler';
 import React from 'react';
-import {SafeAreaView, StyleSheet, View, Text, StatusBar} from 'react-native';
+import {StyleSheet, View, Text, StatusBar} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
+
+const HomeScreen = () => {
+  return (
+    <View style={styles.body}>
+      <Text style={styles.welcomeText}>Welcome</Text>
+    </View>
+  );
+};
 
 const App = () => {
   return (
-    <>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
       <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <View style={styles.body}>
-          <Text style={styles.welcomeText}>Welcome</Text>
-        </View>
-      </SafeAreaView>
-    </>
+    </NavigationContainer>
   );
 };
 
 const styles = StyleSheet.create({
   body: {
+    flex: 1,
     backgroundColor: '#ffffff',
   },
   welcomeText: {
